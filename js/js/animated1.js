3c834a44adb2eee45903049ae336e0cdd5af1b70
petrovkor <30798805+petrovkor@users.noreply.github.com>
2017-08-07 18:02:43 +0300

    Delete animated1.js

--- js/js/animated1.js
+++
@@ -1,9 +0,0 @@
- $(window).scroll(function (){
-                 $(' .mov1').each(function (){
-                 var imagePos = $(this).offset().top;
-                 var topOfWindow = $(window).scrollTop();
-                if (imagePos < topOfWindow+450) {
-                $(this).addClass('bounceInLeft');
-            }
-        });
-    });
